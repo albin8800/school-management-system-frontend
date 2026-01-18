21c4210a84dfe40ac4cb494b15a1566618b5a40a
Albin B Thomas <albinbthomas8800@gmail.com>
2026-01-19 01:17:40 +0530

    bg color

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -9,7 +9,7 @@ export default function AdminLayout({ children }) {
 
       <div className="flex-1 flex flex-col min-h-screen">
         <Topbar />
-        <main className="flex-1 bg-gray-100 pl-6 pt-10">
+        <main className="flex-1 bg-[#F9F9F9] pl-6 pt-10">
           {children}
         </main>
       </div>
